16e5d970cdcd247edd177ab0e2839be8a1cdf5c0
Emon <saifullahemo@gmail.com>
2021-01-27 18:27:22 +0600

    holaa

--- custom.js
+++ custom.js
@@ -1,18 +1,61 @@
-// document.addEventListener('click', 
-// function(){
-//     document.getElementById('button');
-//     document.querySelector('.bg-modal').style.display = 'flex';
-// });
-// console.log("hello");
+$(document).ready(function(e){
 
-document.getElementById("buttonId").addEventListener("click", function(){
-    
-    // document.querySelector(".bg-modal").style.display = "unset";
-    document.querySelector(".bg-modal").style.display = "flex";
-    // document.querySelector(".bg-modal").style.zIndex = "12";
+    /**
+     * On Clicking on Sign Up Button
+     */
+    $("#sign_up").on("click", function(e){
+        e.preventDefault();
+        $(".bg-modal").hide()
+
+        Message.add('Thank you for signing up',{
+            vertical:'top',
+            horizontal:'center'
+        });
+
+    })
+
+    /**
+     * On Clicking Close Button
+     */
+    $(".close").on('click', function(e){
+        $(".bg-modal").css("display","none");
+    })
+
+    /**
+     * On Clicking Modal Show Button
+     *   */
+    $("#buttonId").on('click', function(e){
+        console.log("I am being clicked");
+        $(".bg-modal").css("display","flex");
+    })
+
+    window.halloweenBats = $.halloweenBats({
+
+        // path to the image
+        image: '/assets/images/bats.png',
+      
+        // total amount of bats
+        amount: 15, // Bat amount.
+      
+        // image width
+        width: 35,
+      
+        // frame height
+        height: 20,
+      
+        // total amount of frames
+        frames: 2, 
+      
+        // animation speed
+        speed: 20,
+      
+        // Higher value = slower
+        flickering: 15,
+
+        zIndex: 10000,
+
+      });
+
+
 });
 
-document.querySelector(".close").addEventListener("click", 
-function(){
-    document.querySelector(".bg-modal").style.display = "none";
-});
